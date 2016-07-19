ad2d9654a1721be614adb0b55042328321fcaaf7
Joshua Harms <joshua@nozzlegear.com>
2016-07-19 10:46:42 -0500

    Version 1.4.0

---
+++ tests/usage_charges.ts
@@ -0,0 +1,14 @@
+/// <reference path="./../typings/index.d.ts" />
+
+import {expect} from "chai";
+import {UsageCharges, UsageCharge} from "../index";
+import * as config from "./_utils";
+
+describe("UsageCharges", function ()
+{
+    it ("should list charges, but cannot be tested with a private app.");
+    
+    it ("should get a charge, but cannot be tested with a private app.")
+    
+    it ("should create a charge, but cannot be tested with a private app.");
+})
