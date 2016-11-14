68cf56042d0bf3bf9914501308c859e844548965
Joshua Harms <joshua@nozzlegear.com>
2016-11-14 14:52:02 -0600

    Remove built tests

--- tests/built/charges.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-describe("Charges", function () {
-    it("should list charges, but cannot be tested with a private app.");
-    it("should get a charge, but cannot be tested with a private app.");
-    it("should create a charge, but cannot be tested with a private app.");
-    it("should activate a charge, but cannot be tested with a private app.");
-});
