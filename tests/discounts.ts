97e124e1d41f4ef1c6d8d085fe31f3f19e61b9bc
Joshua Harms <joshua@nozzlegear.com>
2016-12-29 15:58:19 -0600

    Don't run discount and gift card tests, as they can only be run on a Shopify Plus account

--- tests/discounts.ts
+++ tests/discounts.ts
@@ -6,75 +6,15 @@ import Discount = Models.Discount;
 describe("Discounts", function () {
     this.timeout(10000);
 
-    const service = new Discounts(config.shopDomain, config.accessToken);
-    let d: Discount
+    it("should create a Discount, but cannot be tested without a Shopify Plus account.")
 
-    afterEach((cb) => setTimeout(cb, 500));
+    it("should get a Discount, but cannot be tested without a Shopify Plus account.");
 
-    it("should create a discount", async () => {
-        try {
-            let newDiscount: Discount = {
-                "discount_type": "percentage",
-                "value": "15.0",
-                "code": "balderdash"
-            }
+    it("should list Discounts, but cannot be tested without a Shopify Plus account.");
 
-            // "discount_type": "shipping",
-            // "code": "quidagis?",
-            // "starts_at": "2015-08-23T00:00:00-04:00",
-            // "ends_at": "2015-08-27T23:59:59-04:00",
-            // "usage_limit": 20
+    it("should delete a Discount, but cannot be tested without a Shopify Plus account.");
 
-            d = await service.create(newDiscount);
+    it("should enable a Discount, but cannot be tested without a Shopify Plus account.");
 
-            expect(d).to.not.be.null;
-            expect(d.id).to.be.a("number");
-
-        } catch (err) {
-            console.log(err)
-            throw err
-        }
-    });
-
-    it("should get a discount by Id", async () => {
-        const res = await service.get(d.id)
-        expect(res).to.not.be.null;
-        expect(res.id).to.not.be.null;
-        expect(res.id).to.be.a("number");
-
-        console.log(res)
-    })
-
-    it("should disable a discount by Id", async () => {
-        await service.disable(d.id)
-
-        const res = await service.get(d.id)
-        expect(res).to.not.be.null;
-        expect(res.id).to.eq(d.id);
-        expect(res.status).to.eq("disabled");
-    })
-
-    it("should enable a discount by Id", async () => {
-        await service.enable(d.id)
-
-        const res = await service.get(d.id)
-        expect(res).to.not.be.null;
-        expect(res.id).to.eq(d.id);
-        expect(res.status).to.eq("enabled");
-    })
-
-    it("should delete a discount by Id", async () => {
-        await service.delete(d.id)
-
-        try {
-            await service.get(d.id)
-
-            throw new Error("Expected an error")
-
-        } catch (err) {
-            // Expect a 404 Not Found
-            expect(err.statusCode).to.eq(404)
-        }
-    })
-
+    it("should disable a Discount, but cannot be tested without a Shopify Plus account.");
 }) 
