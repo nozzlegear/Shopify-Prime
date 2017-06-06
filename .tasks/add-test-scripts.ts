ad58d34ad809ab2676dbbbc062e0e632f2f69954
Joshua Harms <joshua@nozzlegear.com>
2017-06-06 16:05:04 -0500

    Add script to generate single test scripts.

---
+++ .tasks/add-test-scripts.ts
@@ -0,0 +1,25 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+type Package = { scripts: { [name: string]: string } };
+
+const pkg: Package = require("../package.json");
+const testFiles = fs.readdirSync(path.join(__dirname, "../tests"));
+
+testFiles.forEach(fileName => {
+    const fileInfo = path.parse(fileName); 
+
+    if (fileInfo.name === "_utils") {
+        return;
+    }
+
+    const scriptName = `test:${fileInfo.name}`;
+    const rawScriptName = `${scriptName}:raw`;
+    const rawScriptValue = `alsatian -T ./bin/tests/${fileInfo.name}.js`;
+    const scriptValue = `${rawScriptValue} | tap-bark`;
+
+    pkg.scripts[scriptName] = scriptValue;
+    pkg.scripts[rawScriptName] = rawScriptValue;
+})
+
+fs.writeFileSync(path.join(__dirname, "../package.json"), JSON.stringify(pkg, null, 4));
