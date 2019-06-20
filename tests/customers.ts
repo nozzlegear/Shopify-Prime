358ae6529cf5c6462ad472b8aea742ed121008b7
Robert Wittman <robert.wittman@webpt.com>
2019-06-19 18:46:25 -0700

    Add tests for customer resources

--- tests/customers.ts
+++ tests/customers.ts
@@ -10,11 +10,11 @@ import {
     } from 'alsatian';
 import { Config, Expect } from './test_utils';
 
-@TestFixture("CustomCollection Tests")
-export class CustomCollectionTests {
-    private service = new Prime.CustomCollections(Config.shopDomain, Config.accessToken);
+@TestFixture("Customer Tests")
+export class CustomerTests {
+    private service = new Prime.Customers(Config.shopDomain, Config.accessToken);
 
-    private created: Prime.Models.CustomCollection[] = [];
+    private created: Prime.Models.Customer[] = [];
 
     @AsyncTeardownFixture
     private async teardownAsync() {
@@ -24,43 +24,97 @@ export class CustomCollectionTests {
 
         // Wait 3 seconds after all tests to let the API rate limit bucket empty.
         inspect("Waiting 3 seconds to let API rate limit empty.")
-        
+
         await new Promise(resolve => setTimeout(() => {
             inspect("Continuing.")
             resolve();
         }, 3000));
     }
 
-    private async create(scheduleForDeletion = true) {
-        // const obj = await this.service.create({
-        //     title: "Shopify Prime Test Collection - " + Date.now(), 
-        // });
-
-        // if (scheduleForDeletion) {
-        //     this.created.push(obj);
-        // };
-
-        // return obj;
-        return {} as Prime.Models.CustomCollection;
-    }
-
-    @AsyncTest("should count collections")
+    @AsyncTest("should count customers")
     @Timeout(5000)
-    public async Test1() {
+    public async TestCount() {
         const count = await this.service.count();
-        
+
         Expect(count).toBeGreaterThan(0);
     }
 
-    @AsyncTest("should list collections")
+    @AsyncTest("should list customers")
     @Timeout(5000)
-    public async Test2() {
+    public async TestList() {
         const list = await this.service.list();
-        
+
         Expect(list).toBeAnArray();
-        Expect(list).itemsToPassValidator<Prime.Models.CustomCollection>(i => {
+        Expect(list).itemsToPassValidator<Prime.Models.Customer>(i => {
             Expect(i).toBeType("object");
             Expect(i.id).toBeGreaterThan(0);
         })
+    }
+
+    private async create(email, scheduleForDeletion = true) {
+        const obj = await this.service.create({
+            email: email,
+            first_name: "Test",
+            last_name: "User"
+        });
+        if (scheduleForDeletion) {
+            this.created.push(obj);
+        }
+        return obj;
+    }
+
+    @AsyncTest("should create a customer")
+    @Timeout(5000)
+    public async TestCreate() {
+        const customer = await this.create("createtest@gmail.com");
+
+        Expect(customer).toBeType("object");
+        Expect(customer.email).toEqual("createtest@gmail.com");
+        Expect(customer.first_name).toEqual("Test");
+        Expect(customer.last_name).toEqual("User");
+        Expect(customer.state).toEqual("disabled");
+    }
+
+    @AsyncTest("should update a customer")
+    @Timeout(5000)
+    public async TestUpdate() {
+        const customerId = (await this.create("updatetest@gmail.com")).id;
+
+        const email = "updatedemail@gmail.com";
+        const first_name = "NewTest";
+        const last_name = "NewUser";
+        const updatedCustomer = await this.service.update(customerId, {
+          email,
+          first_name,
+          last_name
+        });
+
+        Expect(updatedCustomer).toBeType("object");
+        Expect(updatedCustomer.email).toEqual(email);
+        Expect(updatedCustomer.first_name).toEqual(first_name);
+        Expect(updatedCustomer.last_name).toEqual(last_name);
+    }
+
+    @AsyncTest("should delete a customer")
+    @Timeout(5000)
+    public async TestDelete() {
+      const id = (await this.create("testdelete@gmail.com", false)).id;
+      let error;
+
+      try {
+        await this.service.delete(id);
+      } catch (e) {
+        error = e;
+      }
+
+      Expect(error).toBeNullOrUndefined();
+    }
+
+    @AsyncTest("should generate an activation url")
+    @Timeout(5000)
+    public async TestCreateActivationUrl() {
+        const id = (await this.create("testactivation@gmail.com")).id;
+        const url = await this.service.createActivationUrl(id);
+        Expect(url).toContain(`${Config.shopDomain}/account/activate/`);
     }
 }
