66a00f1b72c2f82ebdbd2945698daaf0f8068b42
Robert Wittman <robert.wittman@webpt.com>
2019-06-19 18:49:54 -0700

    Make customer search options optional

--- options/customers.ts
+++ options/customers.ts
@@ -2,10 +2,10 @@ export interface CustomerSearchOptions {
   /**
    * Text to search for in the shop's customer data.
    */
-  query: string;
+  query?: string;
 
   /**
    * Set the field and direction by which to order results.
    */
-  order: string;
+  order?: string;
 }
